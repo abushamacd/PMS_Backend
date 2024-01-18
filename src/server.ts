a53d1dae2faa83e79cd7a34b16dec10bff749b79
abushamacd <assiddik001@gmail.com>
2024-01-18 16:32:33 +0600

    ready for deploy

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 import { errorLogger, logger } from './utilities/logger'
 import { bootStrap } from './utilities/bootStrap'
